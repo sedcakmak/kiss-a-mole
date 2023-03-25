338b213fb9a9a6fde99c4c831ac22455c85a566e
Sedef Cakmak <cakmaksedef@gmail.com>
2023-03-25 18:21:44 +0300

    added mechanics

--- index.js
+++ index.js
@@ -1,46 +1,41 @@
-let moles = document.querySelectorAll(".mole-img");
-const startButton = document.querySelector(".start-button");
 let timerEle = document.querySelector("#timer");
-const holes = document.querySelectorAll(".mound");
-const scoreBoard = document.querySelector("#score");
-
+let grounds = document.querySelectorAll(".ground");
+const score = document.querySelector("#score");
 let timeUp = false;
-let lastMole;
-let score = 0;
+let timerImg = document.querySelector(".timer-img");
+let smackImg = document.querySelector(".smack");
 
-startButton.addEventListener("click", startGame);
+window.addEventListener("load", () => {
+  document.querySelector(".start-button").addEventListener("click", () => {
+    startGame();
+  });
+});
 
+let lastGround;
 function randomTime(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
 
-function randomMole(moles) {
-  const idx = Math.floor(Math.random() * moles.length);
-  const mole = moles[idx];
-  if (mole === lastMole) {
+function randomGround(grounds) {
+  const idx = Math.floor(Math.random() * grounds.length);
+  const ground = grounds[idx];
+  if (ground === lastGround) {
     console.log("Ah nah thats the same one bud");
-    return randomMole(moles);
+    return randomGround(grounds);
   }
-  lastMole = mole;
-  return mole;
+  lastGround = ground;
+  return ground;
 }
 
 function peep() {
   const time = randomTime(400, 2000); //200, 1000
-  const mole = randomMole(moles);
-  mole.classList.add("up");
+  const ground = randomGround(grounds);
+  ground.classList.add("active");
   setTimeout(() => {
-    mole.classList.remove("up");
+    ground.classList.remove("active");
+    smackImg.classList.remove("popup");
     if (!timeUp) peep();
   }, time);
-}
-
-function startGame() {
-  scoreBoard.textContent = 0;
-  timerEle.innerHTML = 30;
-  score = 0;
-  peep();
-  countdown();
 }
 
 function countdown() {
@@ -51,86 +46,41 @@ function countdown() {
     if (timeleft === 0) {
       clearInterval(countdown);
       timeUp = true;
+      timerImg.classList.add("popup");
     }
-  }, 30000);
+  }, 1000);
 }
 
-function bonk(e) {
-  // if (!e.isTrusted) return; // cheater!
-  score++;
-  console.log(e.target);
-  this.parentNode.classList.remove("up");
-  scoreBoard.textContent = score;
-}
+const startGame = () => {
+  timerEle.innerHTML = 30;
+  let count = 0;
+  peep();
+  countdown();
 
-moles.forEach((mole) => mole.addEventListener("click", bonk));
+  const length = grounds.length;
 
-// function enterMoles() {
-//   if (!timeUp) {
-//     moles.forEach((mole) => mole.classList.remove("anim"));
-//     const randomNumbers = Array.from({ length: 6 }, () =>
-//       Math.floor(Math.random() * 6)
-//     );
-//     const time = randomTime(200, 1000);
+  grounds.forEach((e) => {
+    e.addEventListener("click", () => {
+      //If ground has active class which means it has mole
+      //So increase the count
+      if (e.classList.contains("active")) {
+        smackImg.classList.add("popup");
+        count++;
+        score.innerHTML = count;
+      }
+    });
+  });
 
-//     randomNumbers.forEach((number) => {
-//       setTimeout(function () {
-//         moles[number].classList.add("anim");
-//       }, 1000);
-//     });
-//   }
-// }
+  // var interval = setInterval(() => {
+  //   //Generate a random number
+  //   const random = Math.floor(Math.random() * length);
 
-//   let numbers = [];
-//   while (numbers.length < moles.length) {
-//     let random = Math.floor(Math.random() * moles.length) + 1;
-//     if (!numbers.includes(random)) {
-//       numbers.push(random);
-//     }
-//   }
-//   console.log(numbers);
+  //   //Remove the active class from every ground
+  //   grounds.forEach((e) => {
+  //     e.classList.remove("active");
+  //   });
 
-//   numbers.forEach((number) => {
-//     moles[number].classList.add("anim");
-//   });
-// }
-
-// let lastMole;
-// function randomMole() {
-//   let mole = moles[Math.floor(Math.random() * moles.length)];
-//   if (mole === lastMole) {
-//     // console.log("Ah nah thats the same one bud");
-//     return randomHole(moles);
-//   }
-//   lastMole = mole;
-//   console.log(mole);
-//   mole.classList.add("anim");
-// }
-//randomMole();
-
-// function startGame() {
-//   console.log("calisiyor mu??");
-//   countdown();
-//   setInterval(enterMoles, 3000);
-// }
-
-// function randomTime(min, max) {
-//   return Math.round(Math.random() * (max - min) + min);
-// }
-
-// function enterMoles() {
-//   const randomNumbers = Array.from({ length: 6 }, () =>
-//     Math.floor(Math.random() * 6)
-//   );
-//   const time = randomTime(200, 1000);
-
-//   randomNumbers.forEach((number) => {
-//     moles[number].classList.add("up");
-//   });
-//   setTimeout(function () {
-//     moles.forEach((mole) => {
-//       mole.classList.remove("up");
-//       if (!timeup) enterMoles();
-//     });
-//   }, time);
-// }
+  //   //Add the active class to random ground
+  //   grounds[random].classList.add("active");
+  // }, 700);
+};
