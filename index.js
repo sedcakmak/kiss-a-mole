2680d3b89de8da5312db8a530823305e23295d61
Sedef Cakmak <cakmaksedef@gmail.com>
2023-03-26 12:47:37 +0300

    added final modal

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 let timerEle = document.querySelector("#timer");
 let grounds = document.querySelectorAll(".ground");
 const score = document.querySelector("#score");
+const finalScore = document.querySelector("#final-score");
 let timeUp = false;
 let timerImg = document.querySelector(".timer-img");
 let smackImg = document.querySelector(".smack");
+let kisses = document.querySelectorAll(".kiss");
 
 window.addEventListener("load", () => {
   document.querySelector(".start-button").addEventListener("click", () => {
@@ -34,6 +36,10 @@ function peep() {
   setTimeout(() => {
     ground.classList.remove("active");
     smackImg.classList.remove("popup");
+    kisses.forEach((kiss) => {
+      kiss.style.opacity = "0";
+      kiss.classList.remove("animate");
+    });
     if (!timeUp) peep();
   }, time);
 }
@@ -47,6 +53,8 @@ function countdown() {
       clearInterval(countdown);
       timeUp = true;
       timerImg.classList.add("popup");
+
+      modal.style.display = "block";
     }
   }, 1000);
 }
@@ -65,8 +73,13 @@ const startGame = () => {
       //So increase the count
       if (e.classList.contains("active")) {
         smackImg.classList.add("popup");
+        kisses.forEach((kiss) => {
+          kiss.style.opacity = "1";
+          kiss.classList.add("animate");
+        });
         count++;
         score.innerHTML = count;
+        finalScore.innerHTML = count;
       }
     });
   });
@@ -83,4 +96,31 @@ const startGame = () => {
   //   //Add the active class to random ground
   //   grounds[random].classList.add("active");
   // }, 700);
+};
+
+// FINALE MODAL
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 };
